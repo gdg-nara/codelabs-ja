74c00461e33cb508dea96320f4814e38aeecaacc
t-cool <hiroki802@hotmail.com>
2021-02-25 17:22:29 +0900

    updated: 2021_0225_17:22

--- build.sh
+++ build.sh
@@ -6,12 +6,10 @@ cd ./android/fundamentals2/unit1
 claat export 1PPfXc5VLN0m_DOBVQzbDfssKBbjC67PNRIkWW9d3QWY
 cd ../../../
 
-
-TODAY=`date "+%Y%m%d%h%s"`
-
+# GitHub へのcommit
+commitTime=`date +%Y_%m%d_%H:%M`
+commitMessage="updated: ${commitTime}"
 git add .
-
-git commit -m ${TODAY}
-
+git commit -m ${commitMessage}
 git push -u origin master
-
+echo ${commitMessage}
