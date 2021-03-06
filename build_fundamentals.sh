cea152e4ff792e55db6787f4d63a9b8b7740b9d2
t-cool <hiroki802@hotmail.com>
2021-03-06 09:39:31 +0900

    claat の挙動に合わせてドキュメントを再生成

--- build_fundamentals.sh
+++ build_fundamentals.sh
@@ -9,9 +9,9 @@ claat export 1ZS4wqGHj7XcOd-lOiI8h4g6lBYOBVGEaKcIwMM8fp10
 # 1-2B
 claat export 1YC1a5pOeyXdCm-16MrzkdPb7LjocozAI89xprK7Uk7A
 # 1-3
-claat export 1loK0v6MyyL08UOtlCzkicmJ6JDBiPxgnoBgOoOUi2RE
+claat export 10DstyEivA3KAYzzUW1mGhGA-gnBOltQ89BnB-nG7i60
 # 1-4
-claat export 1gEbKw9vm8Ez8LOqqr5Y49G3Djge4ZMaqc8vkLlGpr3U
+claat export 1kiCwvDjqg95KynvxbE_KEVcenHq3r93KSzB728XwzbE
 # 2-1
 # claat export 1NLmVv-iwtuPUJ8qJwNGDdPxLmcZOJvRIMneVvfw4iFc
 # 2-2
