2420336ff3b13de9a96758e7e22aab7dbcfdfcce
t-cool <hiroki802@hotmail.com>
2021-03-08 16:01:05 +0900

    Lesson1 - 10 の機械翻訳全てをpush

--- build_fundamentals.sh
+++ build_fundamentals.sh
@@ -13,55 +13,55 @@ claat export 10DstyEivA3KAYzzUW1mGhGA-gnBOltQ89BnB-nG7i60
 # 1-4
 claat export 1kiCwvDjqg95KynvxbE_KEVcenHq3r93KSzB728XwzbE
 # 2-1
-# claat export 1NLmVv-iwtuPUJ8qJwNGDdPxLmcZOJvRIMneVvfw4iFc
+claat export 1NLmVv-iwtuPUJ8qJwNGDdPxLmcZOJvRIMneVvfw4iFc
 # 2-2
-# claat export 1q4_noZdNyMOL4ZPWfrWXz7pdU0u-MPJukI_Jyshzkcw
+claat export 1q4_noZdNyMOL4ZPWfrWXz7pdU0u-MPJukI_Jyshzkcw
 # 2-3
-# claat export 1HgD1pzqiVyXy11_eutktXhcbQf_4SHSvgyhEGHLlvfk
+claat export 1HgD1pzqiVyXy11_eutktXhcbQf_4SHSvgyhEGHLlvfk
 # 3-1
-# claat export 1W4LCgbH-TZJ9DG-nwhGf3DoErWyFhR2AVPxsWx1wMUw
+claat export 1W4LCgbH-TZJ9DG-nwhGf3DoErWyFhR2AVPxsWx1wMUw
 # 3-2
-# claat export 167wgllv_JSixiuF7L3kFhEmKAoKvSGgmTcx3k72-deg
+claat export 167wgllv_JSixiuF7L3kFhEmKAoKvSGgmTcx3k72-deg
 # 3-3
-# claat export 1RXv54-ht6PBOTsX3dr3KR-GYG4e0LtMXJOVVOBB8PaA
+claat export 1RXv54-ht6PBOTsX3dr3KR-GYG4e0LtMXJOVVOBB8PaA
 # 4-1
-# claat export 1mP4GPhUmG8tz81_XXbuFXl38FTop1m6VhEezYnEfk2U
+claat export 1mP4GPhUmG8tz81_XXbuFXl38FTop1m6VhEezYnEfk2U
 # 4-2
-# claat export 11-LlFXGaW9oekcMCdGyqjSpBF2bjh2AnWeTvlXv2Wxk
+claat export 11-LlFXGaW9oekcMCdGyqjSpBF2bjh2AnWeTvlXv2Wxk
 # 4-3
-# claat export 1gYhuyTJG_DzFAS0ObARzIfmXxz01critDNfws-J3dMU
+claat export 1gYhuyTJG_DzFAS0ObARzIfmXxz01critDNfws-J3dMU
 # 4-4
-# claat export 1leu_lMed-TDKD1zh1RaF5GfkyANJvvm2cdjKYCegntE
+claat export 1leu_lMed-TDKD1zh1RaF5GfkyANJvvm2cdjKYCegntE
 # 4-5
-# claat export 1ai1hu4Vts2Kg0EjUvjryO192JTnAmwJPT6zuJYcB-KM
+claat export 1ai1hu4Vts2Kg0EjUvjryO192JTnAmwJPT6zuJYcB-KM
 # 5-1
-# claat export 1D2ngZwxJsaApZdbW9SCdei_nZh83Tbwjc8xPVZH1uXQ
+claat export 1D2ngZwxJsaApZdbW9SCdei_nZh83Tbwjc8xPVZH1uXQ
 # 5-2
-# claat export 1Ab6KBJbLkwlEroorPjKHpIGxtGb2wU-O6d5UlGisj3c
+claat export 1Ab6KBJbLkwlEroorPjKHpIGxtGb2wU-O6d5UlGisj3c
 # 5-3
-# claat export 1ZKysI5gkPYSVAxvA58udybftryXxT-kX3cmIcdG3i3M
+claat export 1pUbPJFPlj5owfRBzHzHC0q_yKUsXx4RNdJ-rOxRfSLc
 # 6-1
-# claat export 13YrV0zZbZ9qtFFASajrF0emQyCqiuerebp0PMTgtsv0
+claat export 13YrV0zZbZ9qtFFASajrF0emQyCqiuerebp0PMTgtsv0
 # 7-1
-# claat export 1LpUR7POz8O8vn4e6Ib62CpjZ1-eU2qBNO4TktJgZRbE
+claat export 1LpUR7POz8O8vn4e6Ib62CpjZ1-eU2qBNO4TktJgZRbE
 # 7-2
-# claat export 1sPzvIl0Z6hV_in9qlaVmsDqsUs_F5gNR1EeTF-D2EXs
+claat export 1sPzvIl0Z6hV_in9qlaVmsDqsUs_F5gNR1EeTF-D2EXs
 # 7-3
-# claat export 14IcZJBTMjy6Bg5n8zisgbGA_D1aU2xpwz9Koaa51Sro
+claat export 14IcZJBTMjy6Bg5n8zisgbGA_D1aU2xpwz9Koaa51Sro
 # 8-1
-# claat export 1U6nFYXCcFDh3rLcswQa-qRFeSAwPJu3kDwiNRaFoftc
+claat export 1U6nFYXCcFDh3rLcswQa-qRFeSAwPJu3kDwiNRaFoftc
 # 8-2
-# claat export 1G2Wn9d0JlEfUpu3hzX2_sT4lrSgBzboAPuXy_Er3Nvc
+claat export 1G2Wn9d0JlEfUpu3hzX2_sT4lrSgBzboAPuXy_Er3Nvc
 # 8-3
-# claat export 1knHpFBG3UQytnVywsjuDGKHSyKckLHCWMfbdrGSqLjs
+claat export 1knHpFBG3UQytnVywsjuDGKHSyKckLHCWMfbdrGSqLjs
 # 9-1
-# claat export 1Gqc8lKFH0xcHR1M5AnMgfLgInWOwWrZDKhWU45a5sOQ
+claat export 1Gqc8lKFH0xcHR1M5AnMgfLgInWOwWrZDKhWU45a5sOQ
 # 9-2
-# claat export 1hMRk_Ui17Br5ycJXZxVHmujKpZ7JxyYjhAlV3RzSwuo
+claat export 1hMRk_Ui17Br5ycJXZxVHmujKpZ7JxyYjhAlV3RzSwuo
 # 10-1A
-# claat export 1SBYXrGO-oNuMypOA1yUsEwJRKPui8LFPKOL8itlw-7Q
+claat export 1SBYXrGO-oNuMypOA1yUsEwJRKPui8LFPKOL8itlw-7Q
 # 10-1B
-# claat export 1LVC90OODzFzMU9xoJkfSyOqgUbzF9NADY3u2h0ZHm8E
+claat export 1LVC90OODzFzMU9xoJkfSyOqgUbzF9NADY3u2h0ZHm8E
 
 cd ../../
 
